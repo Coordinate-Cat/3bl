713958f43ff03154ed7dda1553f61715fd4dae71
Coordinate-Cat <tetrapasta02@gmail.com>
2022-09-03 14:02:03 +0900

    feat: optimize

--- README.md
+++ README.md
@@ -10,5 +10,30 @@ or
 - example
   - https://notetoself-dy.com/blender-threejs-gltf/
 
+## model is dirty?
+https://ics.media/tutorial-three/renderer_resize/
+
+```javascript
+onResize();
+// リサイズイベント発生時に実行
+window.addEventListener('resize', onResize);
+
+function onResize() {
+  // サイズを取得
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  // レンダラーのサイズを調整する
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+
+  // カメラのアスペクト比を正す
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+}
+```
+
+
 ## asset
-- https://sketchfab.com/3d-models/among-us-map-the-skeld-59a93886f9e74ff6836dff0c269da45f
+- amongus
+  - https://sketchfab.com/3d-models/among-us-map-the-skeld-59a93886f9e74ff6836dff0c269da45f
--- three.js
+++ three.js
@@ -1,24 +1,24 @@
-// import
+// -------------- import ----------------
 import * as THREE from'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/GLTFLoader.js';
 
-// variables
+// -------------- variables ----------------
 let camera;
 let scene;
 let renderer;
 let model;
 
-// func
+// ---------------- init() ----------------
 init();
-animate();
 function init() {
-  //シーンの作成
+  // シーンの作成
   scene = new THREE.Scene();
 
-  //カメラの作成
+  // カメラの作成
   camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-  //カメラセット
+
+  // カメラセット
   camera.position.set(-20, 30, 50);
   camera.lookAt(new THREE.Vector3(0, 10, 0));
 
@@ -27,13 +27,13 @@ function init() {
   controls.enableDamping = true;
   controls.dampingFactor = 0.2;
 
-  //光源
-  const dirLight = new THREE.SpotLight(0xffffff,1.5);//color,強度
+  // 光源
+  const dirLight = new THREE.SpotLight(0xffffff, 2.5); //color,強度
   dirLight.position.set(-20, 30, 30);
   scene.add(dirLight);
-  scene.background = new THREE.Color( 0x000000 );
+  scene.background = new THREE.Color(0x000000);
 
-  //レンダラー
+  // レンダラー
   renderer = new THREE.WebGLRenderer({
     alpha: true,
     antialias: true
@@ -41,26 +41,47 @@ function init() {
   renderer.setClearColor(new THREE.Color(0xffffff));
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  //glbファイルの読み込み
+  // glbファイルの読み込み
   const loader = new GLTFLoader();
-
   loader.load('./model/amongus.glb', function(gltf) {
     model = gltf.scene;
-    model.traverse((object) => { //モデルの構成要素
-      if(object.isMesh) { //その構成要素がメッシュだったら
-      object.material.trasparent = true;//透明許可
-      object.material.opacity = 0.8;//透過
-      object.material.depthTest = true;//陰影で消える部分
+    model.traverse((object) => {           //モデルの構成要素
+      if(object.isMesh) {                  //その構成要素がメッシュだったら
+        object.material.trasparent = true; //透明許可
+        object.material.opacity = 0.8;     //透過
+        object.material.depthTest = true;  //陰影で消える部分
       }})
     scene.add(model);
   }, undefined, function(e) {
     console.error(e);
   });
 
+  // htmlのid取得
   document.getElementById("WebGL-output").appendChild(renderer.domElement);
 }
 
+// ---------------- onResize() ----------------
+onResize();
+// リサイズイベント発生時に実行
+window.addEventListener('resize', onResize);
+
+function onResize() {
+  // サイズを取得
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  // レンダラーのサイズを調整する
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+
+  // カメラのアスペクト比を正す
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+}
+
+// ---------------- animate() ----------------
+animate();
 function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
